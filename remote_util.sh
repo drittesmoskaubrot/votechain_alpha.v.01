cdc3e021b4b3e5042f9e26a3a816d95a6deab1cd
Pi Dev <drittesmoskaubrot@gmail.com>
2017-02-17 18:08:07 +0000

    fixed compiler flag for older machines

--- remote_util.sh
+++ remote_util.sh
@@ -1,3 +1,3 @@
-g++ -c vc_shell.cpp
-g++ -c remote_utils.cpp
-g++ vc_shell.o remote_utils.o -o remote_utils
+g++ -c -std=c++11  vc_shell.cpp
+g++ -c  -std=c++11 remote_utils.cpp
+g++  -std=c++11 vc_shell.o remote_utils.o -o remote_utils

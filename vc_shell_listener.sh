db75297cca5570a7280d76feb7fc4e413e7b33bf
13116371 <drittesmoskaubrot.com>
2017-02-18 22:23:19 +0000

    minor bug fix

--- vc_shell_listener.sh
+++ vc_shell_listener.sh
@@ -3,7 +3,7 @@ while [ true ]
 do
 echo "[*] test"
 cd  ~/git/votechain_alpha.v.01/
-./remote_utils -h "Dell Optiplex-755 agent" -n -s
+./remote_utils -h "Acer | v5  agent" -n -s
 sleep 60
 # just in case something weird happens
 done

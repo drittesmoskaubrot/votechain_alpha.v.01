b728beff5c96044f69a2b5656b163dba901419e1
13116371 <drittesmoskaubrot.com>
2017-02-18 21:21:55 +0000

    creating individual branches

--- vc_shell_listener.sh
+++ vc_shell_listener.sh
@@ -3,7 +3,7 @@ while [ true ]
 do
 echo "[*] test"
 cd  ~/git/votechain_alpha.v.01/
-./remote_utils -h "Acer Aspire | v5 agent" -n -s
+./remote_utils -h "HP 32bit agent" -n -s
 sleep 60
 # just in case something weird happens
 killall mosquitto_sub

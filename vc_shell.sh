93521fa8146e9bcf25d6cc22d00dddd3f468a1c7
13116371 <drittesmoskaubrot.com>
2017-02-18 20:54:50 +0000

    removing harcoded paths what a muppet I am

--- vc_shell.sh
+++ vc_shell.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while [ true ]
 do
-  cd  /home/rogue/git/votechain_alpha.v.01
+  cd  ~/git/votechain_alpha.v.01
   ./remote_utils -h "Acer Aspire | v5 agent" -tmp_agent
   sleep 60
 done
--- vc_shell_listener.sh
+++ vc_shell_listener.sh
@@ -2,7 +2,7 @@
 while [ true ]
 do
 echo "[*] test"
-cd  /home/rogue/git/votechain_alpha.v.01/
+cd  ~/git/votechain_alpha.v.01/
 ./remote_utils -h "Acer Aspire | v5 agent" -n -s
 sleep 60
 # just in case something weird happens
